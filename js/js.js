7804db4401acd89d821f57449bccea057627324e
AntoChiape <113721708+AntoChiape@users.noreply.github.com>
2022-11-14 16:43:14 +0100

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,8 @@
+function oscuro() {
+  document.body.style.backgroundColor = "black";
+  }
+
+
+  function copy() {
+    alert("ANTOCHIAPE WEB DESIGN Argentina | Estados Unidos | EUR | contacto@antochiapeicloud.com")
+  }
